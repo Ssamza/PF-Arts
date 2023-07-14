0167047d21883a828c8dd838c8239432e249e3b8
Ssamza <santiagoemezeta@gmail.com>
2023-07-14 12:26:54 -0500

    revert change

--- src/handlers/artworks/getHandler.js
+++ src/handlers/artworks/getHandler.js
@@ -11,7 +11,7 @@ const getArtworkHandler = async (req, res) => {
       });
 
       if (artworkName.length === 0) {
-        return []	;
+        throw Error('Artwork not available');
       } else {
         res.status(200).json(artworkName);
       }
@@ -23,7 +23,7 @@ const getArtworkHandler = async (req, res) => {
       });
 
       if (author.length === 0) {
-        return [];
+        throw Error('User/Artist not available');
       } else {
         res.status(200).json(author);
       }
